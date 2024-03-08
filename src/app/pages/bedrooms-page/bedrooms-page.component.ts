8cb37f0801acd594a920521ffa0b0b4c0d5eea9b
Haziel Ortiz <ya-yo22124@outlook.com>
2024-03-08 10:03:57 -0600

    Add: Bedrooms page and service

---
+++ src/app/pages/bedrooms-page/bedrooms-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bedrooms-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './bedrooms-page.component.html',
+  styleUrl: './bedrooms-page.component.scss'
+})
+export class BedroomsPageComponent {
+
+}
