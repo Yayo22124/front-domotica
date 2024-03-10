9901cb727674cb4815a79aba594148f38a2b969e
MendezPro <orlanmen58@gmail.com>
2024-03-10 10:47:27 -0600

    upd: Actualizando servicios y componentes

--- src/app/pages/bedrooms-page/bedrooms-page.component.ts
+++ src/app/pages/bedrooms-page/bedrooms-page.component.ts
@@ -34,7 +34,7 @@ export class BedroomsPageComponent implements OnInit {
       (params) => {
         this.bedroomName = params.get('location');
 
-        this.getBedroomData(this.bedroomName!)
+        this.getBedroomData(this.bedroomName!) 
       }
     )
   }
