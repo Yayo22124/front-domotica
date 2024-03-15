8eb58c2764bd4cb40d2b90896d3e02acff667897
ZaS-ex <a.zacek.gc@gmail.com>
2024-03-15 12:26:50 -0600

    add: Subiendo componente de proximidad; upd: Actualizando componentes de la pagina

--- src/app/pages/bathrooms-page/bathrooms-page.component.ts
+++ src/app/pages/bathrooms-page/bathrooms-page.component.ts
@@ -8,10 +8,16 @@ import { apiUrl } from '../../core/constants/apiUrl.constant';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { ActivatedRoute } from '@angular/router';
+import { PhotoresistorDataItemComponent } from '../../components/photoresistor-data-item/photoresistor-data-item.component';
+import { LoadingService } from '../../core/services/Loading/loading.service';
+import { CommonModule } from '@angular/common';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { ProximityDataItemComponent } from '../../components/proximity-data-item/proximity-data-item.component';
 @Component({
   selector: 'app-bathrooms-page',
   standalone: true,
-  imports: [MatCardModule,MatButtonModule],
+  imports: [MatCardModule,MatButtonModule, CommonModule, MatIconModule, MatDividerModule, PhotoresistorDataItemComponent, ProximityDataItemComponent],
   templateUrl: './bathrooms-page.component.html',
   styleUrl: './bathrooms-page.component.scss'
 })
@@ -19,9 +25,19 @@ export class BathroomsPageComponent implements OnInit {
   public sensorsData: iSensorsData[] | undefined =[];
   public actuatorsData: iActuatorsData[] | undefined =[];
   public bathroomName: string | null="";
+
+  public ldrData: iSensorsData | null = null;
+  public presenceData: iSensorsData | null = null;
+  public proximityData: iSensorsData | null = null;
+  public doorData: iActuatorsData | null = null;
+  public inLightData: iActuatorsData | null = null;
+  public exLightData: iActuatorsData | null = null;
+  public waterPumpData: iActuatorsData | null = null;
+
   constructor(
     private bathroomsService: BathroomsService,
     private route: ActivatedRoute,
+    private loadinService: LoadingService
   ) { 
 
   }
@@ -34,11 +50,28 @@ export class BathroomsPageComponent implements OnInit {
       )
   }
   getBathroomsData(location: string){
+    this.loadinService.showLoading()
     this.bathroomsService.getBathroomsData(location).subscribe(
       (response: iApiResponse) => {
         console.log(response);
         this.actuatorsData = response.actuatorsData;
         this.sensorsData = response.sensorsData;
+
+        if(this.actuatorsData && this.sensorsData){
+          this.ldrData = this.sensorsData.find(sensor => sensor.name === 'Fotorresistencia')!;
+          this.presenceData = this.sensorsData.find(sensor => sensor.name === 'Presencia')!;
+          this.proximityData = this.sensorsData.find(sensor => sensor.name === 'Proximidad')!;
+          
+          this.doorData = this.actuatorsData.find(actuator => actuator.name === 'Servomotor Puerta')!;
+          this.inLightData = this.actuatorsData.find(actuator => actuator.name === 'Led Interior')!;
+          this.exLightData = this.actuatorsData.find(actuator => actuator.name === 'Led Exterior')!;
+          this.waterPumpData = this.actuatorsData.find(actuator => actuator.name === 'Mini Bomba de Agua Sumergible')!;
+        }
+
+        this.loadinService.hideLoading();
+      }, (error) => {
+        this.loadinService.hideLoading();
+        console.error(error);
       }
     )
   }
