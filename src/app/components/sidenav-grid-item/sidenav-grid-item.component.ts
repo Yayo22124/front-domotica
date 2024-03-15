80c49608bab783adbaa02786df0478494f2573e8
Haziel Ortiz <ya-yo22124@outlook.com>
2024-03-14 18:55:33 -0600

    Upd: Sidenav, Header and Siednav Item design

--- src/app/components/sidenav-grid-item/sidenav-grid-item.component.ts
+++ src/app/components/sidenav-grid-item/sidenav-grid-item.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidenav-grid-item',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './sidenav-grid-item.component.html',
   styleUrl: './sidenav-grid-item.component.scss'
 })
---
+++ src/app/components/temperature-data-item/temperature-data-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-temperature-data-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './temperature-data-item.component.html',
+  styleUrl: './temperature-data-item.component.scss'
+})
+export class TemperatureDataItemComponent {
+
+}
