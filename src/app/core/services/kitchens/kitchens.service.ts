d4eb43511ccc0d4e68bde586cd2e0d40fb0b134b
IrvingM <irvonm92@gmail.com>
2024-03-18 10:27:26 -0600

    Six commit

---
+++ src/app/core/services/kitchens/kitchens.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { apiUrl } from '../../constants/apiUrl.constant';
+import { iApiResponse } from '../../interfaces/i-ApiResponse';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class KitchensService {
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getAllKitchens(): Observable<iApiResponse> {
+    return this.http.get<iApiResponse>(`${apiUrl}/kitchen/`)  
+  }
+
+  getKitchenData(location: string): Observable<iApiResponse> {
+    return this.http.get<iApiResponse>(`${apiUrl}/kitchen/?location=${location}`)  
+  }
+}
