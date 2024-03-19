e7f3d418d8c4a03112b86658dd5679cebdd3cbe5
Aldo Tolentino <tolentinodomingodiego@gmail.com>
2024-03-19 15:44:07 -0600

    Slider de puerta

--- src/app/components/door-data-item/door-data-item.component.ts
+++ src/app/components/door-data-item/door-data-item.component.ts
@@ -5,11 +5,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { iActuatorsData } from '../../core/interfaces/i-ActuatorsData.interface';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-door-data-item',
   standalone: true,
-  imports: [MatCardModule, MatTooltipModule, MatButtonModule, MatIconModule, CommonModule],
+  imports: [MatCardModule, MatTooltipModule, MatButtonModule, MatIconModule, CommonModule, MatSlideToggle],
   templateUrl: './door-data-item.component.html',
   styleUrl: './door-data-item.component.scss'
 })
