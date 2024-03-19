c4010b011af30b1c57f74aacd777e26624b57ec1
Haziel Ortiz <ya-yo22124@outlook.com>
2024-03-19 16:05:15 -0600

    Fix contant apiUrl

--- src/app/core/constants/apiUrl.constant.ts
+++ src/app/core/constants/apiUrl.constant.ts
@@ -1,2 +1,2 @@
-export const apiUrl = "http://localhost:3000/api/v1";
-//  export const apiUrl = "https://api-utxj-home.up.railway.app/api/v1";
+// export const apiUrl = "http://localhost:3000/api/v1";
+ export const apiUrl = "https://api-utxj-home.up.railway.app/api/v1";
