9fb3bb57fa9b3e2a6ee5e96f7a865b8a4774104d
ZaS-ex <a.zacek.gc@gmail.com>
2024-03-21 13:03:54 -0600

    upd: añadiendo funcion de 'ver-mas' a baños

--- src/app/components/presence-data-item/presence-data-item.component.ts
+++ src/app/components/presence-data-item/presence-data-item.component.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { Router } from '@angular/router';
 import { iSensorsData } from '../../core/interfaces/iSensorsData.interface';
 
 @Component({
@@ -21,5 +22,18 @@ import { iSensorsData } from '../../core/interfaces/iSensorsData.interface';
   styleUrl: './presence-data-item.component.scss',
 })
 export class PresenceDataItemComponent {
+
+  constructor(private router: Router) {}
+
   @Input('presence-data') presenceData: iSensorsData | null = null;
+
+  @Input('component-room-name') componentRoomName: string | null = null;
+  @Input('component-location') componentLocation: string | null = null;
+  @Input('component-name') componentName: string | null = null;
+
+  seeMore() {
+    this.router.navigate([
+      `${this.componentRoomName}/sensor/${this.componentLocation}/${this.componentName}`,
+    ]);
+  }
 }
