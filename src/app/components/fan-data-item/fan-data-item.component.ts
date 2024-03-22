e25c324edaccc5de8e58855a4dbac5cbece0abe7
ZaS-ex <a.zacek.gc@gmail.com>
2024-03-21 20:27:54 -0600

    upd: muchos

--- src/app/components/fan-data-item/fan-data-item.component.ts
+++ src/app/components/fan-data-item/fan-data-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -8,14 +9,65 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 
 import { iActuatorsData } from '../../core/interfaces/i-ActuatorsData.interface';
+import { ComponentControlService } from '../../core/services/ComponentControl/component-control.service';
+import { LoadingService } from '../../core/services/Loading/loading.service';
 @Component({
   selector: 'app-fan-data-item',
   standalone: true,
-  imports: [MatCardModule, MatTooltipModule, MatButtonModule, MatIconModule, CommonModule, MatSlideToggleModule],
+  imports: [MatCardModule, MatTooltipModule, MatButtonModule, MatIconModule, CommonModule, MatSlideToggleModule, MatSnackBarModule],
   templateUrl: './fan-data-item.component.html',
   styleUrl: './fan-data-item.component.scss'
 })
 export class FanDataItemComponent {
-  @Input("fan-data") fanData:
-  iActuatorsData | null = null;
+
+  constructor(
+    private componentService: ComponentControlService, 
+    private loadingService: LoadingService, 
+    private _snackbar: MatSnackBar) {
+    
+  }
+
+  @Input("fan-data") fanData: iActuatorsData | null = null;
+  public isLoading: boolean = false;
+
+  fanOn(arduinoIp:string){
+    this.isLoading=true;
+    this.loadingService.showLoading();
+    this.componentService.fanOn(arduinoIp).subscribe(
+      (res)=>{
+        this.loadingService.hideLoading();
+        this.isLoading = false;
+        this._snackbar.open("Ventilador Encendido", "Cerrar", {
+          duration: 2.5*1000
+        });
+      },
+      (err) => {
+        this.loadingService.hideLoading();
+        this.isLoading = false;
+        this._snackbar.open("Error al apagar el Ventilador", "Cerrar", {
+          duration: 2.5*1000
+        });
+      }
+    )
+  }
+  fanOff(arduinoIp:string){
+    this.isLoading=true;
+    this.loadingService.showLoading();
+    this.componentService.fanOff(arduinoIp).subscribe(
+      (res)=>{
+        this.loadingService.hideLoading();
+        this.isLoading = false;
+        this._snackbar.open("Ventilador Apagado", "Cerrar", {
+          duration: 2.5*1000
+        });
+      },
+      (err) => {
+        this.loadingService.hideLoading();
+        this.isLoading = false;
+        this._snackbar.open("Error al apagar el Ventilador", "Cerrar", {
+          duration: 2.5*1000
+        });
+      }
+    )
+  }
 }
